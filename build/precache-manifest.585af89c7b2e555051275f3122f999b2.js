cd81a260b330562194653ce45a7b8213d64ef1f7
YoungJun Park <youngjun108059@gmail.com>
2022-08-08 02:25:01 +0900

    🔖 첫번째 Build

---
+++ build/precache-manifest.585af89c7b2e555051275f3122f999b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650c86cbe3f8db33c7bf14cbf9dbbf86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bb4c3e7089083bc805b",
+    "url": "/static/css/main.33000e73.chunk.css"
+  },
+  {
+    "revision": "2b7ec75e45a87098c880",
+    "url": "/static/js/2.67b82299.chunk.js"
+  },
+  {
+    "revision": "6b9496458fffbef6ad84b631d3aeaee9",
+    "url": "/static/js/2.67b82299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993ce46cc055cebd7d27",
+    "url": "/static/js/3.43c7a152.chunk.js"
+  },
+  {
+    "revision": "1bb4c3e7089083bc805b",
+    "url": "/static/js/main.b0d293a8.chunk.js"
+  },
+  {
+    "revision": "98a1a7913d1f18e58563",
+    "url": "/static/js/runtime-main.cf2e0c13.js"
+  }
+]);
